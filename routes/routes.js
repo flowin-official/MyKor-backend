3c526eea73826ce33d2ea2e8488616e1e8fda84b
박근원 <root-park@baggeun-won-ui-MacBookPro.local>
2024-05-15 17:00:49 +0900

    FEAT)API) users/:userId/posts/:postId

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { ping } from '../controller/system.js';
 import { getAllUsersHandler, getUserByIdHandler } from '../controller/handler/user-handler.js';
-import { getPostByUserIdHandler } from '../controller/handler/post-handler.js';
+import { getPostByUserIdHandler, getPostInfoByUserIdPostIdHandler } from '../controller/handler/post-handler.js';
 
 const setupRoutes = (app) => {
   const router = express.Router();
@@ -11,6 +11,7 @@ const setupRoutes = (app) => {
   router.route('/users').get(getAllUsersHandler);
   router.route('/users/:userId').get(getUserByIdHandler);
   router.route('/users/:userId/posts').get(getPostByUserIdHandler);
+  router.route('/users/:userId/posts/:postId').get(getPostInfoByUserIdPostIdHandler);
 
   app.use('/mykor/api/v1', router);
 };
